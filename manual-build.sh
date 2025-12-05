1a3e6f71d6eeb474efdd9e1d1fdc5f6488ca39b6
Alasdair Macmillan <aimmac23@gmail.com>
2025-12-05 19:10:30 +0000

    Update script to build multi-arch images

--- manual-build.sh
+++ manual-build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Manual build image command
-# TODO: Setup Github actions if this gets used enough
+# Build multi-arch image
+# NOTE: RUN commands need to run on the real architecture, so borrowing the arm64 built images from a self-hosted build pipeline
+# TODO: Automate more if this needs to be re-build frequently
 
-podman build -t aimmac23/conan-server:latest --layers .
+podman manifest create  docker.io/aimmac23/conan-server
 
+podman build -t  aimmac23/conan-server:latest-amd64 --layers --platform linux/amd64  .
+
+podman manifest add docker.io/aimmac23/conan-server aimmac23/conan-server:latest-amd64
+# From self-hosted Forgejo instance
+podman manifest add docker.io/aimmac23/conan-server docker.aimmac23.com/aim/conan-server:latest
+
+
+#podman manifest push docker.io/aimmac23/conan-server
